5455f243807077fef5d7344797fcad98d07b4c26
dolfi1 <38071588+dolfi1@users.noreply.github.com>
2026-03-02 20:07:00 +0500

    Remove unused Scrapy scaffolding from portable build

--- nadin_scrapy/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Scrapy-based company/leader extraction package."""
-
-from .items import CompanyLeaderItem
-from .pipelines import CompanyProfilePipeline
-
-__all__ = ["CompanyLeaderItem", "CompanyProfilePipeline"]
